92291a3d46325cf07b1391dd3bb413eb4d60ef57
luoxiapeng <949123073@qq.com>
2019-12-11 13:55:57 +0800

    增加vConsole

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/route'
+    '~/plugins/route',
+    { src: '~/plugins/vconsole', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
